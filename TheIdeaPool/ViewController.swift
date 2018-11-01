f25135eb3f723b281e466f4e4e42f0c0a43c3987
Dan <dandurbaca@dans-macbook.local>
2018-11-01 09:19:21 +0200

    Initial Commit

---
+++ TheIdeaPool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TheIdeaPool
+//
+//  Created by Dan Durbaca on 01/11/2018.
+//  Copyright © 2018 Dan Durbaca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
